6d94d23e3dbc7637f1dfb93a59da307d388b1f88
alexvc <alexvoncampe@stmarksschool.org>
2013-05-09 09:21:25 -0400

    Slightly Bigger Object

--- JavaGroupGameSpring2013/src/AlexvcObject.java
+++ JavaGroupGameSpring2013/src/AlexvcObject.java
@@ -7,17 +7,17 @@ public class AlexvcObject extends GameObject{
 	private int x = xLoc;
 	private int y = yLoc;
 	
-	private int[] xList = {x-25, x+25, x, x-25, x+25};
-	private int[] yList = {y-25, y-25, y, y+25, y+25};
+	private int[] xList = {x-30, x+30, x, x-30, x+30};
+	private int[] yList = {y-30, y-30, y, y+30, y+30};
 
 
 	public AlexvcObject(String shapeName, int w, int h){
 		
 		super(shapeName, w, h);
 		
-		this.setBBoxExtras(0, -1, 50, 51);
-		 objW = 50;
-		 objH = 50;
+		this.setBBoxExtras(0, -1, 60, 61);
+		 objW = 60;
+		 objH = 60;
 		
 		
 	}
@@ -25,17 +25,17 @@ public class AlexvcObject extends GameObject{
 	public void drawObject(Graphics g){
 		
 		
-		xList[0] = xLoc -25;
-		xList[1] = xLoc +25;
+		xList[0] = xLoc -30;
+		xList[1] = xLoc +30;
 		xList[2] = xLoc;
-		xList[3] = xLoc -25;
-		xList[4] = xLoc +25;
+		xList[3] = xLoc -30;
+		xList[4] = xLoc +30;
 		
-		yList[0] = yLoc -25;
-		yList[1] = yLoc -25;
+		yList[0] = yLoc -30;
+		yList[1] = yLoc -30;
 		yList[2] = yLoc;
-		yList[3] = yLoc +25;
-		yList[4] = yLoc +25;
+		yList[3] = yLoc +30;
+		yList[4] = yLoc +30;
 		
 		g.setColor(Color.RED);
 		g.drawRect(xLoc-(int)(objW/2)+bBoxExtraX, yLoc-(int)(objH/2)+bBoxExtraY,bBoxW, bBoxH);
